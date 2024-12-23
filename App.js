914c4191b72b90c055e12a790abb0ae4d3763bda
Sithsdoc <jos.sardina@gmail.com>
2024-12-23 15:52:50 -0500

    random quotes button

--- App.js
+++ App.js
@@ -1,88 +1,52 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, View, Button } from "react-native";
+import { StyleSheet, View, Button, Text } from "react-native";
 import { FlatList } from "react-native";
 import { CheckBox, Input } from "@rneui/themed";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-let initTasks = [
-  { description: "Task 1", completed: true, key: "1" },
-  { description: "Task 2", completed: false, key: "2" },
+let initQuotes = [
+  { quote: "I am your father", title: "first quote" },
+  { quote: "May the force be with you", title: "second quote" },
+  { quote: "That laddie, is the sound of a dragon", title: "third quote" },
+  { quote: "One ring to rule them all", title: "fourth quote" },
+  {
+    quote: "People linked by destiny will always find each other",
+    title: "fifth quote",
+  },
 ];
 
 export default function App() {
-  const [input, setInput] = useState("");
-  const [tasks, setTasks] = useState(initTasks);
+  //sets this to hold 0 quotes so whatever is pulled from the randomizer will be pulled first
+  const [quotes, setQuotes] = useState(null);
 
-  const updateTask = (taskToUpdate) => {
-    const updatedTasks = tasks.map((task) => {
-      if (task.key === taskToUpdate.key) {
-        return { ...task, completed: !task.completed };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
+  const getRandomQuote = () => {
+    //the first half of the code takes a random number between 1 and 0 and mulitplies it be length which is then rounded down by math.floor
+    const randomIndex = Math.floor(Math.random() * initQuotes.length);
+    //assigns the current quote to setQuotes
+    setQuotes(initQuotes[randomIndex]);
   };
 
-  const addTask = () => {
-    const maxKey = Math.max(...tasks.map((task) => Number(task.key)), 0) + 1;
-    setTasks([
-      ...tasks,
-      {
-        description: input,
-        completed: false,
-        key: maxKey.toString(),
-      },
-    ]);
-    setInput("");
-  };
-
-  const renderItem = ({ item }) => {
-    return (
-      <CheckBox
-        title={item.description}
-        checked={item.completed}
-        onPress={() => updateTask(item)}
-        iconType="material-community"
-        checkedIcon={
-          <MaterialCommunityIcons
-            name="checkbox-marked"
-            size={24}
-            color="green"
-          />
-        }
-        uncheckedIcon="checkbox-blank-outline"
-        textStyle={
-          item.completed
-            ? {
-                textDecorationLine: "line-through",
-                textDecorationStyle: "solid",
-              }
-            : {}
-        }
-      />
-    );
-  };
-
+  //.quote comes from the initquotes quote property
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <FlatList
-        data={tasks}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.key.toString()}
+      {quotes && <Text style={styles.quoteText}>{quotes.quote}</Text>}
+      <Button
+        title="Press to get random quote"
+        onPress={() => {
+          getRandomQuote();
+        }}
       />
-      <View style={styles.horizontal}>
-        <Input
-          onChangeText={setInput}
-          value={input}
-          placeholder="...Enter task"
-        />
-        <Button onPress={addTask} title="Add Task" />
-      </View>
     </View>
   );
 }
+/*
+steps to create quote randomizer
+1. create a screen that displays 1 of the 5 quotes
+2. create a button that the user will click to randomize the selection of quotes
+3. design an algorithm that will randomize which quote (assign numbers and use math.random?)
+4. create a shared quote button?
+ */
 
 const styles = StyleSheet.create({
   container: {
@@ -95,6 +59,12 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     padding: 10,
+  },
+  quoteText: {
+    fontSize: 18,
+    fontStyle: "italic",
+    marginBottom: 20,
+    textAlign: "center",
   },
 });
 //https://reactnative.dev/docs/checkbox
